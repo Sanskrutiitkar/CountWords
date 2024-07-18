f0ac801ca102dc5123abeeb4a94f0a63c8539a0b
Sanskruti Itkar <139566431+Sanskrutiitkar@users.noreply.github.com>
2024-07-18 20:05:22 +0530

    Add files via upload

---
+++ CountWords/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a sentence: ");
+        string sentence = Console.ReadLine();
+
+        int wordCount = 0;
+        bool wordContd = false;
+
+        //iterate through each letter 
+        for (int i = 0; i < sentence.Length; i++)
+        {
+            //logic to check double spacing 
+            if (sentence[i] != ' ')
+            {
+                if (!wordContd)
+                {
+                    wordCount++;
+                    wordContd = true;
+                }
+            }
+            else
+            {
+                wordContd = false;
+            }
+        }
+        Console.WriteLine($"Number of words: {wordCount}");
+    }
+}
